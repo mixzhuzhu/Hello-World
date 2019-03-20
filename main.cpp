9683f5b41c8c3b6ff0752883bc2bd90ff4fe6a61
zhuhoucong <zhuhoucong@126.com>
2019-03-20 21:09:53 +0800

    Revert "push mastaer to dev"
    This reverts commit c2a23ab8b649ba6b1d095d3290750a7968ea6e46.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc,char** argv)
 {
-    printf("git22 master2 Hello world\n");
+    printf("git22 master Hello world\n");
+    printf("git22  dev Hello world\n");
 
     return 0;
 }
